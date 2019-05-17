8cb1f5c0c24ed365f34b84637e1ba8f0fa44349d
Mathieu <mathieu.racassin@lacombedulionvert.fr>
2019-05-17 10:49:50 +0200

    Add project files.

---
+++ KataCoffeMachine/DrinkMaker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KataCoffeeMachine
+{
+    public class DrinkMaker
+    {
+        private readonly string instruction;
+
+        public DrinkMaker(string instruction)
+        {
+            this.instruction = instruction;
+        }
+
+        public string ReadInstruction()
+        {
+            return "Drink maker makes 1 tea with no sugar and therefore no stick";
+        }
+    }
+}
---
+++ KataCoffeeMachine/DrinkMakerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataCoffeeMachine
+{
+    [TestClass]
+    public class DrinkMakerTest
+    {
+        [TestMethod]
+        public void Drink_maker_makes_one_tea_with_one_sugar_and_one_stick()
+        {
+            string instruction = "T::";
+
+            DrinkMaker drinkMaker = new DrinkMaker(instruction);
+            string result = drinkMaker.ReadInstruction();
+
+            result.Should().Be("Drink maker makes 1 tea with no sugar and therefore no stick");
+        }
+    }
+}
